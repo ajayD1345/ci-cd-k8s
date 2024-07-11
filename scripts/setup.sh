d2eea41650b2bafbc34b0612c5405b0450068837
hamed Ayojide <hamedayojide58@gmail.com>
2024-07-11 05:15:48 +0100

    Added new folders

---
+++ scripts/setup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Update/Upgrade the package index
+sudo apt update -y
+sudo apt upgrade -y
+
+# Add Docker's official GPG key:
+sudo apt install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings -y
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc -y
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+
+# Install the docker packages
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Verify installation of docker
+sudo docker run hello-world
+
+# Install Git
+ sudo apt install git -y
+
+# Install Ansible
+ sudo apt install software-properties-common -y
+ sudo add-apt-repository --yes --update ppa:ansible/ansible
+ sudo apt install ansible -y
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl -y  /usr/local/bin/kubectl
+kubectl version --client
+
+# Install Terraform
+ sudo apt install -y gnupg software-properties-common
+ wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install terraform -y
+
+# Install Jenkins
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install jenkins -y
+sudo apt install fontconfig openjdk-17-jre -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+java -version
+openjdk version "17.0.8" 2023-07-18
+OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+
+# Allow Jenkins to use Docker
+#sudo usermod -aG docker jenkins
+sudo systemctl restart jenkins
+
+# Print setup completion message
+echo "Setup successful"
